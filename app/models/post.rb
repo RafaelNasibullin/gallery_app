525b61e48b2e9cb2666e7193625d6ff4e27db10d
RubyBorei <human1241@gmail.com>
2017-04-01 16:02:17 +0600

    Added validations to Post module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
 	mount_uploader :image, ImageUploader
-  belongs_to :user
-end
+	belongs_to :user
+	validates :title, presence: true
+	validates :image, format: { with: /[a-zA-Z0-9]*.(jpg|png)/,
+		message: "only allows letters" }
+		validates :image, presence: true
+
+
+	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 	root "posts#index"
   resources :posts
-  resources :users
+
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
